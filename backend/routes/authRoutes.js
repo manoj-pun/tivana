0747d41822c9a559abfd0bcd85d6454695048bba
manoj-pun <manojpun840@gmail.com>
2025-04-30 18:51:59 +0545

    added login and logout user feature

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { registerUser } from "../controllers/authController.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post("/register-user",registerUser)
+router.post("/login-user",loginUser)
+router.post("/logout-user",logoutUser)
 
 export default router
