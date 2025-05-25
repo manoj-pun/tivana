6b426249e1021082fd878320b535bc8d57d76459
manoj-pun <manojpun840@gmail.com>
2025-05-25 17:02:08 +0545

    ---

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB()
 
 connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL];
 
 app.use(cookieParser());
 app.use(express.json())
